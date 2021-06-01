7316fc7ee70c4f50c5168b443c312cab251395ee
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-06-01 15:58:50 -0300

    adiciona requisito 2

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementsByClassName('color')[0].style.backgroundColor = 'red';
+document.getElementsByClassName('color')[1].style.backgroundColor = 'blue';
+document.getElementsByClassName('color')[2].style.backgroundColor = 'yellow';
+document.getElementsByClassName('color')[3].style.backgroundColor = 'purple';
