065eadffab12d68a3c10cbe2038b2d33058b76dd
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-06-02 11:43:30 -0300

    adiciona requisitos 8 e 9

--- script.js
+++ script.js
@@ -4,24 +4,41 @@ document.getElementsByClassName('color')[2].style.backgroundColor = 'yellow';
 document.getElementsByClassName('color')[3].style.backgroundColor = 'purple';
 
 let color1 = document.getElementsByClassName('color');
-color1[0].addEventListener('click', addSelectedClass);
-color1[1].addEventListener('click', addSelectedClass);
-color1[2].addEventListener('click', addSelectedClass);
-color1[3].addEventListener('click', addSelectedClass);
+let selectedColor = document.getElementsByClassName('selected')[0];
+
+color1[0].addEventListener('click', changeSelectedClass);
+color1[1].addEventListener('click', changeSelectedClass);
+color1[2].addEventListener('click', changeSelectedClass);
+color1[3].addEventListener('click', changeSelectedClass);
 
 function removeSelectedClass() {
   for (let index = 0; index < color1.length; index += 1) {
-    if (color1[index].classList.value == 'color selected') {
+    if (color1[index].classList.value === 'color selected') {
       color1[index].className = 'color';
     }
   } 
 }
 
-function addSelectedClass(click) {
+function changeSelectedClass(click) {
   removeSelectedClass();
-  let selectedColor = document.getElementsByClassName('selected')[0];
   click.target.classList.add('selected');
 }
 
+let pixel = document.getElementsByClassName('pixel');
+let board = document.getElementById('pixel-board');
 
+function changeColorPixel(event) {
+  let colorOfSelectedClass = document.getElementsByClassName('selected')[0].style.backgroundColor
+  let clickedPixel = event.target
+  clickedPixel.style.backgroundColor = colorOfSelectedClass
+} 
+board.addEventListener('click', changeColorPixel)
 
+function clearBoard() {
+  for (let index = 0; index < pixel.length; index += 1) {
+    pixel[index].style.backgroundColor = 'white';
+  } 
+}
+
+let button = document.getElementById('clear-board')
+button.addEventListener('click', clearBoard)
